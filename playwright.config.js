2e2aec9eb2f447a3c34fe9881a24bdb0ba09c884
shahnawaz <shahnawaz@empirepixel.com>
2026-07-24 23:12:35 +0500

    Update Slack notification header for Playwright CI

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@playwright/test');
+const path = require('path');
 module.exports = defineConfig({ 
   testDir: './tests',
   fullyParallel: true,
   workers: '50%',
   retries: 1,
-  reporter: [['html', { open: 'never' }], ['list'], ['json', { outputFile: 'results.json' }]],
+  reporter: [['html', { open: 'never' }], ['list']],
   timeout: 60000,
   use: {
     trace: 'off',
